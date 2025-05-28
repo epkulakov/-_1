8267522af70dfc1a3c479cfa3c84f24a53e63064
epkulakov <epkulakov@yandex.ru>
2025-05-29 00:49:26 +0500

    Add files via upload

---
+++ Django_regectrat/user_registration/accounts/views.py
@@ -0,0 +1,18 @@
+from tempfile import template
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from accounts.forms import RegistrationForm
+
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
